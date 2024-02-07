98c89445e633e91111cf03ba01a8f1725a52f086
Allyson Torres <allysonnamikase@gmail.com>
2024-02-06 22:10:50 -0300

    Começando o curso

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+     
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+  
