d7992f2bb079941cfe3d58a6d615850928de7ea1
Allyson Torres <allysonnamikase@gmail.com>
2024-02-19 22:33:44 -0300

    Atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Tarefas"),
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(
-                height: 140,
-                color: Colors.blue,
-              ),
-              Container(
-                height: 100,
-                color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      height: 100,
-                      width: 70,
-                    ),
-                    Text("Aprender Flutter"),
-                    ElevatedButton(
-                      onPressed: () {}, 
-                      child: Icon(Icons.arrow_drop_up),),
-                  ],
-                ),
-              ),
-            ],
-          ),
+        body: ListView(
+          children: const [
+            Task("Aprender flutter no cafe da manhã"),
+            Task("Andar de bike"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
@@ -50,12 +44,89 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-class Task extends StatelessWidget {
-  const Task({super.key});
+class Task extends StatefulWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: 140,
+              color: Colors.blue,
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        height: 100,
+                        width: 70,
+                      ),
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.nome,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+                          print(nivel);
+                        },
+                        child: Icon(Icons.arrow_drop_up),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel / 10,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Nivel: $nivel",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ), 
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
