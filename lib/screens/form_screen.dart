f6a75681a6e5a08be8498081b189d8a66bd7c017
Allyson Torres <allysonnamikase@gmail.com>
2024-03-08 14:41:08 -0300

    Estilizando form Screen

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -9,6 +9,9 @@ class FormScreen extends StatefulWidget {
 
 class _FormScreenState extends State<FormScreen> {
   TextEditingController nameControlller = TextEditingController();
+  TextEditingController difficultyControlller = TextEditingController();
+  TextEditingController imageControlller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +22,17 @@ class _FormScreenState extends State<FormScreen> {
         child: Container(
           height: 700,
           width: 395,
-          decoration: BoxDecoration(color: Colors.black12, borderRadius: BorderRadius.circular(10), border: Border.all(color: Colors.black, width: 3)),
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: Colors.black,
+              width: 3,
+            ),
+          ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -30,12 +42,10 @@ class _FormScreenState extends State<FormScreen> {
                     border: OutlineInputBorder(),
                     hintText: "Nome",
                     fillColor: Colors.white70,
-                    filled: true
+                    filled: true,
                   ),
                   controller: nameControlller,
                 ),
-                 
-                 
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -43,18 +53,58 @@ class _FormScreenState extends State<FormScreen> {
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: "Nome",
+                    hintText: "Dificuldade",
                     fillColor: Colors.white70,
-                    filled: true
+                    filled: true,
                   ),
-                  controller: nameControlller,
+                  controller: difficultyControlller,
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  onChanged: (text) {
+                    setState(() {
+                      
+                    });
+                  },
+                  textAlign: TextAlign.center,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Imagem",
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                  controller: imageControlller,
+                ),
+              ),
+              Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageControlller.text,
+                      errorBuilder: (context, error, stackTrace) {
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset("assets/images/vetor.png"),);
+                      },
+                      fit: BoxFit.cover,
+                    ),
+                  )),
               ElevatedButton(
-                onPressed: (){
+                onPressed: () {
                   print(nameControlller.text);
-                }, 
-                child: Text("Adcionar"),
+                  print(int.parse(difficultyControlller.text));
+                  print(imageControlller.text);
+                },
+                child: const Text("Adcionar"),
               )
             ],
           ),
